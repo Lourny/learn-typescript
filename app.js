066e7e998e6d7d2837f96dd318bd12e8e57ae531
masoud_jalili <masoudjalili0781@gmail.com>
2024-11-20 12:01:15 +0330

    exersice typescript and learn about types in typescript

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var message = "hello world";
-console.log(message);
+//create finding element
+var heading = document.createElement("h1");
+heading.textContent = message;
+//apeend child to dom
+document.body.appendChild(heading);
+function getProduct(id) {
+    {
+        return {
+            id: id,
+            name: "the text for id = ".concat(id),
+            price: 99.56,
+        };
+    }
+}
+heading.requer();
--- app.ts
+++ app.ts
@@ -1,2 +1,24 @@
-let message:string="hello world";
-console.log(message)
+let message: string = "hello world";
+
+//create finding element
+let heading = document.createElement("h1");
+heading.textContent = message;
+
+//apeend child to dom
+document.body.appendChild(heading);
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+function getProduct(id: number): Product {
+  {
+    return {
+      id: id,
+      name: `the text for id = ${id}`,
+      price: 99.56,
+    };
+  }
+}
